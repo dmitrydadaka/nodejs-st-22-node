5f511b804e3904424c9182d3557ada7db5654af5
User <belpc2012@gmail.com>
2022-07-18 13:07:02 +0300

    feat: create branch task-2-1  and adde task

---
+++ task1.js
@@ -0,0 +1,18 @@
+const { pipeline, Transform } = require('stream');
+
+const reverse = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.reverse() + '\n');
+  },
+});
+try {
+  pipeline(
+    process.stdin,
+    reverse,
+    process.stdout,
+    err => console.log(err)
+  )
+}
+catch (err) {
+  console.log(err);
+}
